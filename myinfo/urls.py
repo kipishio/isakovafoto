59d5e3421a08b4817a20b27a8d206c46071f8e68
kipishio1 <kipishio@yandex.ru>
2015-10-11 04:06:56 +0600

    Подключил еще один шаблон, но он толком  не работает

--- myinfo/urls.py
+++ myinfo/urls.py
@@ -2,8 +2,36 @@
 __author__ = 'Юля'
 
 from django.conf.urls import patterns, url
-from myinfo.views import index
+from myinfo import views
 
 urlpatterns = patterns('',
-                       url(r'^$', index),
+                       url(r'^$', views.index),
+                       url(r'^architect/$', views.indexart, name='indexart'),
+                       url(r'^architect/index.html/$', views.indexart, name='indexart'),
+                       url(r'^architect/blog_four.html/$', views.architect_blog_four, name='architect_blog_four'),
+                       url(r'^architect/blog_one.html/$', views.architect_blog_one, name='architect_blog_one'),
+                       url(r'^architect/blog_three.html/$', views.architect_blog_three, name='architect_blog_three'),
+                       url(r'^architect/blog_two.html/$', views.architect_blog_two, name='architect_blog_two'),
+                       url(r"^architect/contact.html/$", views.architect_contact, name='architect_contact'),
+                       url(r'^architect/pages_404.html/$', views.architect_pages_404, name='architect_pages_404'),
+                       url(r'^architect/pages_grid.html/$', views.architect_pages_grid, name='architect_pages_grid'),
+                       url(r'^architect/pages_sample.html/$', views.architect_pages_sample,
+                           name='architect_pages_sample'),
+                       url(r'^architect/pages_shortcodes.html/$', views.architect_pages_shortcodes,
+                           name='architect_pages_shortcodes'),
+                       url(r'^architect/portfolio_four.html/$', views.architect_portfolio_four,
+                           name='architect_portfolio_four'),
+                       url(r'^architect/portfolio_portfolio_one.html/$', views.architect_portfolio_one,
+                           name='architect_portfolio_one'),
+                       url(r'^architect/portfolio_three.html/$', views.architect_portfolio_three,
+                           name='architect_portfolio_three'),
+                       url(r'^architect/portfolio_two.html/$', views.architect_portfolio_two,
+                           name='architect_portfolio_two'),
+
+                       url(r'^architect/services.html/$', views.architect_services,
+                           name='architct_services'),
+                       url(r'^architect/team.html/$', views.architect_team,
+                           name='architect_team'),
+
+
                        )
--- myinfo/views.py
+++ myinfo/views.py
@@ -8,3 +8,83 @@ from django.shortcuts import render, render_to_response
 def index(request):
     index = 'index'
     return render_to_response('myinfo/index.html', {'index': index})
+
+
+def indexart(request):
+    index = 'index'
+    return render_to_response('architect/index.html', {'index': index})
+
+
+def architect_blog_four(request):
+    index = 'index'
+    return render_to_response('architect/blog_four.html', {'index': index})
+
+
+def architect_blog_one(request):
+    index = 'index'
+    return render_to_response('architect/blog_one.html', {'index': index})
+
+
+def architect_blog_three(request):
+    index = 'index'
+    return render_to_response('architect/blog_three.html', {'index': index})
+
+
+def architect_blog_two(request):
+    index = 'index'
+    return render_to_response('architect/blog_two.html', {'index': index})
+
+
+def architect_contact(request):
+    index = 'index'
+    return render_to_response('architect/contact.html', {'index': index})
+
+
+def architect_pages_404(request):
+    index = 'index'
+    return render_to_response('architect/pages_404.html', {'index': index})
+
+
+def architect_pages_grid(request):
+    index = 'index'
+    return render_to_response('architect/pages_grid.html', {'index': index})
+
+
+def architect_pages_sample(request):
+    index = 'index'
+    return render_to_response('architect/pages_sample.html', {'index': index})
+
+
+def architect_pages_shortcodes(request):
+    index = 'index'
+    return render_to_response('architect/pages_shortcodes.html', {'index': index})
+
+
+def architect_portfolio_four(request):
+    index = 'index'
+    return render_to_response('architect/portfolio_four.html', {'index': index})
+
+
+def architect_portfolio_one(request):
+    index = 'index'
+    return render_to_response('architect/portfolio_one.html', {'index': index})
+
+
+def architect_portfolio_three(request):
+    index = 'index'
+    return render_to_response('architect/portfolio_three.html', {'index': index})
+
+
+def architect_portfolio_two(request):
+    index = 'index'
+    return render_to_response('architect/portfolio_two.html', {'index': index})
+
+
+def architect_services(request):
+    index = 'index'
+    return render_to_response('architect/services.html', {'index': index})
+
+
+def architect_team(request):
+    index = 'index'
+    return render_to_response('architect/team.htmll', {'index': index})
